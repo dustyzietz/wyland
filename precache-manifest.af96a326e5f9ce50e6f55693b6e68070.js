bafea81eac6b39edb1caae158586dc45ed0a3cd0
Dusty Zietz <dustyzietz@gmail.com>
2019-10-29 23:33:42 -1000

    Updates

---
+++ precache-manifest.af96a326e5f9ce50e6f55693b6e68070.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7586308bf522e32d2a6375d08426919",
+    "url": "/wyland/index.html"
+  },
+  {
+    "revision": "754e562078c7f8021ca9",
+    "url": "/wyland/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c87267ab62e363cd6d87",
+    "url": "/wyland/static/js/2.40c98396.chunk.js"
+  },
+  {
+    "revision": "754e562078c7f8021ca9",
+    "url": "/wyland/static/js/main.8607a219.chunk.js"
+  },
+  {
+    "revision": "7c72348bd78a3f9bf19a",
+    "url": "/wyland/static/js/runtime-main.4080bae7.js"
+  },
+  {
+    "revision": "d1b723e2ef7d3be1ec03d9e78f6e7616",
+    "url": "/wyland/static/media/Pepe1.d1b723e2.jpg"
+  },
+  {
+    "revision": "785de837ca453aa1cda096a7dd0762ac",
+    "url": "/wyland/static/media/pepe2.785de837.jpg"
+  },
+  {
+    "revision": "74405703f412df2595f7e697a470300c",
+    "url": "/wyland/static/media/pepe3.74405703.jpg"
+  },
+  {
+    "revision": "d4833e9379b3e6f32bd1e595bbbfae34",
+    "url": "/wyland/static/media/pepe4.d4833e93.jpg"
+  },
+  {
+    "revision": "4c6f2928d06ddad2a51c57a95a28eb61",
+    "url": "/wyland/static/media/pepe5.4c6f2928.jpg"
+  },
+  {
+    "revision": "b925b0cfec8755d9d21406b99e786532",
+    "url": "/wyland/static/media/pepe6.b925b0cf.jpg"
+  }
+]);
